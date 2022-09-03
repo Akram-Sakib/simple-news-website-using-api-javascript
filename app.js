47dcfa31df60b04bef98db904d7538fd671b2d10
akram-sakib <sayedakramsakib@gmail.com>
2022-09-03 23:56:24 +0600

    :rocket: Finalization

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const categoriesEl = document.getElementById("categories");
 const articlesEl = document.getElementById("articles");
 const categoryItemEl = document.getElementById("category-item");
 const categoryNameEl = document.getElementById("category-name");
+const modalEl = document.getElementById("modal");
 
 // Get All Categories
 const loadCategories = async () => {
@@ -14,8 +15,8 @@ const loadCategories = async () => {
     const categories = await res.json();
     fetchCategories(categories);
   } catch (error) {
-    alert("Failed to fetch Data")
-    return
+    alert("Failed to fetch Data");
+    return;
   }
 };
 
@@ -120,8 +121,10 @@ const fetchBlogs = (categories) => {
                                     <p class="ml-2 text-gray-600 text-xl font-medium">${
                                       category?.total_view || 0
                                     }M</p>
-                                </div>
-                                <i class="fa-solid fa-arrow-right-long text-3xl text-indigo-900 cursor-pointer hidden md:block"></i>
+                                </div>  
+                                <i onclick="getDetails('${
+                                  category._id
+                                }')" class="fa-solid fa-arrow-right-long text-3xl text-indigo-900 cursor-pointer hidden md:block"></i>
                             </div>
                         </div>
                     </div>
@@ -129,18 +132,62 @@ const fetchBlogs = (categories) => {
     articlesEl.appendChild(div);
   });
 };
+const getDetails = async (newsId) => {
+  const categoryUrl = `https://openapi.programming-hero.com/api/news/${newsId}`;
+
+  const res = await fetch(categoryUrl);
+  const category = await res.json();
+  console.log(category.data[0]);
+  const {
+    thumbnail_url,
+    title,
+    details,
+    author: { name, img, published_date },
+    total_view
+  } = category.data[0];
+  const div = document.createElement("div");
+  div.innerHTML = `
+  <div class="flex items-center justify-center min-height-100vh pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+            <div class="fixed inset-0 transition-opacity">
+                <div class="absolute inset-0 bg-gray-900 opacity-75" />
+            </div>
+            <span class="hidden sm:inline-block sm:align-middle sm:h-screen">&#8203;</span>
+            <div class="inline-block align-center bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full overflow-y-visible"
+                role="dialog" aria-modal="true" aria-labelledby="modal-headline">
+                <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4 ">
+                <div class="px-4 py-3 text-right">
+                  <div class="flex flex-row justify-between">
+                                    <div class="flex flex-row">
+                                      <div class="m-1 mr-2 w-12 h-12 flex justify-center items-center rounded-full bg-gray-500 text-xl text-white">
+                                        <img class="rounded-full" src="${img}">
+                                    </div>
+                                    <div class="flex-col">
+                                        <h2 class="text-base font-medium text-left">${name}</h2>
+                                        <h3>${published_date.split(" ")[0]}</h3>
+                                    </div>
+                                    </div>
+                                    <button type="button" class="px-4 py-0 bg-gray-500 text-white rounded hover:bg-gray-700"
+                        onclick="toggleModal()"><i class="fas fa-times"></i></button>
+                                </div>
+                </div>
+                    <div class="pt-4">
+                        <div class="relative">
+                        <img class="w-full h-60 mx-auto" src="${thumbnail_url}" alt="">
+                        <span class="absolute views">${total_view}m views </span>
+                        </div>
+                        <h2 class="text-xl font-medium my-2">${title}</h2>
+                        <p class="text-gray-700 font-normal mb-4">${details}</p>
+                    </div>
+                </div>
+                
+            </div>
+        </div>`;
+
+  modalEl.appendChild(div);
+  toggleModal();
+};
+const toggleModal = () => {
+  document.getElementById("modal").classList.toggle("hidden");
+};
 handleCategoryFilter("08");
 loadCategories();
-
-// const items = [
-//   { name: "Edward", value: 21 },
-//   { name: "Sharpe", value: 37 },
-//   { name: "And", value: 45 },
-//   { name: "The", value: -12 },
-//   { name: "Magnetic", value: 13 },
-//   { name: "Zeros", value: 37 },
-// ];
-
-// // sort by value
-// items.sort((a, b) => a.value - b.value);
-// console.log(items);
